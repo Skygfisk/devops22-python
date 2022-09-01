0a694a34ddcadf0cceb3ee652f09e19fde47e4b5
Odd Jensen <Odd.Jensen@yh.nackademin.se>
2022-09-01 15:04:10 +0200

    Exercises for lesson 2

---
+++ lesson_2/input.py
@@ -0,0 +1,12 @@
+my_str = input("Type a string: ")
+print(my_str)
+
+my_int = input("Type a int: ")
+print(int(my_int)*2)
+
+my_str_2 = input("Type another string: ")
+my_str_2 += my_str_2 
+print(my_str_2)
+
+my_float = input("Type a float: ")
+print(float(my_float)/3.5)
---
+++ lesson_2/print.py
@@ -0,0 +1,12 @@
+my_int = 5
+print(my_int)
+
+my_float = 3.33
+print(my_float)
+
+my_str = "veritas"
+print(my_str)
+
+print(True)
+print(None)
+print(f"{my_int} {my_float} {my_str} {True} {None}")
