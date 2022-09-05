e0a9feceb94f320b0e22faa0c442b572286a7b42
Odd Jensen <Odd.Jensen@yh.nackademin.se>
2022-09-05 13:54:27 +0200

    Did the  caesar exercise

---
+++ lesson_4/caesar.py
@@ -0,0 +1,17 @@
+#Jag pluggar på Nackademin
+#Qhn wsbnnhy we Uhjrhkltpu
+text = input("Text to encode: ")
+s = int(input("Key to use: "))
+result = ""
+for i in range(len(text)):
+        char = text[i]
+  
+        if (char.isupper()):
+            result += chr((ord(char) + s - 65) % 26 + 65)
+
+        elif (char.islower()):
+            result += chr((ord(char) + s - 97) % 26 + 97)
+        else:
+            result += " "
+            
+print(result)
